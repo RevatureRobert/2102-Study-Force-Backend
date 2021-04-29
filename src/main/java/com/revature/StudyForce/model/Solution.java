a07d06ce0708854e596f41128fb5865717790e26
Jl3E <joey.elmblad@gmail.com>
2021-04-29 09:13:02 -0700

    added Solution model

--- src/main/java/com/revature/StudyForce/model/Solution.java
+++ src/main/java/com/revature/StudyForce/model/Solution.java
@@ -1,14 +1,19 @@
 package com.revature.StudyForce.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.sql.Date;
 
 @Entity
 @Table(name = "solution")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Solution {
 
     @Id
@@ -20,15 +25,18 @@ public class Solution {
             fetch = FetchType.LAZY,
             cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "stacktrace_id")
+    @NotNull
     private Stractrace stackTraceId;
 
     @ManyToOne(
             fetch = FetchType.LAZY,
             cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "user_id")
-    private Users usersId;
+    @NotNull
+    private User usersId;
 
     @Column
+    @NotNull
     private String body;
 
     @Column(name = "admin_selected")
