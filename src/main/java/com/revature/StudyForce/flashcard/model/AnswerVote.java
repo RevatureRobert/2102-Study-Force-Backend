661fc21cef1370b7f25cfe354bb6234aedf53037
ely5 <ely5@case.edu>
2021-04-30 13:57:35 -0400

    done w/ answervote model

--- src/main/java/com/revature/StudyForce/flashcard/model/AnswerVote.java
+++ src/main/java/com/revature/StudyForce/flashcard/model/AnswerVote.java
@@ -1,4 +1,29 @@
 package com.revature.StudyForce.flashcard.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "answer_vote")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnswerVote {
+
+    @Id
+    @GeneratedValue
+    private int voteId;
+    private int voteValue;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
