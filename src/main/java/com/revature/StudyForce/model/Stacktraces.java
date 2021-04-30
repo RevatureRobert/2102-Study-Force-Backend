62e68a05fe0b878df0c4642084afa557ccc39292
nshaji24 <ns35@njit.edu>
2021-04-30 10:26:24 -0400

    comments added

--- src/main/java/com/revature/StudyForce/model/Stacktraces.java
+++ src/main/java/com/revature/StudyForce/model/Stacktraces.java
@@ -1,10 +1,7 @@
 package com.revature.StudyForce.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
@@ -19,25 +16,30 @@ import java.sql.Timestamp;
 @Setter
 @Builder
 public class Stacktraces {
-
+    //PRIMARY KEY
     @Id
     @GeneratedValue
     private int stackstraceId;
 
-    //TODO: change ints to Objects
+    //TODO: change ints to Objects once user models are created
+    //ID of user who created stacktrace
     @NotNull
     private int creatorId;
 
+    //the title a user gives to a stacktrace
     @NotBlank
     private String title;
 
+    //the body text of a stacktrace
     @NotBlank
     private String body;
 
-    //TODO: change ints to Objects
+    //TODO: change ints to Objects once technology model is added
+    //The technology that the stacktrace is using.ex JAVA
     @NotNull
     private int technologyId;
 
+    //time of creation of the stacktrace
     @UpdateTimestamp
     private Timestamp creationTime;
 
