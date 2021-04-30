25ad616e445ea0302cf5e868f8eacaf19f62b2c2
anakin115 <anakinkan@gmail.com>
2021-04-30 11:28:59 -0400

    testing for UserDTO

---
+++ src/test/java/com/revature/StudyForce/DTO/UserDTOTest.java
@@ -0,0 +1,65 @@
+package com.revature.StudyForce.DTO;
+
+import com.revature.StudyForce.user.dto.UserDTO;
+import com.revature.StudyForce.user.model.Authority;
+import com.revature.StudyForce.user.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Timestamp;
+
+/**
+ * UserDTO testing to confirm the functionality for the conversion
+ * @author Lok Kan Kung
+ */
+
+@SpringBootTest
+class UserDTOTest {
+
+    @Test
+    void convertUserToUserDTO(){
+
+        Timestamp registrationTime = Timestamp.valueOf ("2021-04-30 10:58:01");
+        Timestamp lastLoginTime = Timestamp.valueOf ("2021-04-30 11:00:01");
+        Authority authority = Authority.USER;
+        User user = new User(0,"testing@gmail.com","password","test","ing",
+                true,true,true,
+                authority,registrationTime,lastLoginTime);
+        UserDTO userDTO = UserDTO.userToDTO ().apply (user);
+
+        Assertions.assertEquals (0,userDTO.getUserId ());
+        Assertions.assertEquals ("testing@gmail.com",userDTO.getEmail ());
+        Assertions.assertEquals ("",userDTO.getPassword ());
+        Assertions.assertEquals ("test",userDTO.getFirstName ());
+        Assertions.assertEquals ("ing",userDTO.getLastName ());
+        Assertions.assertTrue (userDTO.isActive ());
+        Assertions.assertTrue (userDTO.isSubscribedFlashcard ());
+        Assertions.assertTrue (userDTO.isSubscribedStacktrace ());
+        Assertions.assertEquals (authority,userDTO.getAuthority ());
+        Assertions.assertEquals (registrationTime,userDTO.getRegistrationTime ());
+        Assertions.assertEquals (lastLoginTime,userDTO.getLastLogin ());
+    }
+
+    @Test
+    void convertUserDTOToUser(){
+        Timestamp registrationTime = Timestamp.valueOf ("2021-04-30 10:58:01");
+        Timestamp lastLoginTime = Timestamp.valueOf ("2021-04-30 11:00:01");
+        Authority authority = Authority.USER;
+        UserDTO userDTO = new UserDTO (0,"testing@gmail.com","password","test","ing",
+                true,true,true,
+                authority,registrationTime,lastLoginTime);
+        User user = UserDTO.dtoToUser ().apply (userDTO);
+
+        Assertions.assertEquals (0,user.getUserId ());
+        Assertions.assertEquals ("testing@gmail.com",user.getEmail ());
+        Assertions.assertEquals ("password",user.getPassword ());
+        Assertions.assertEquals ("test",user.getFirstName ());
+        Assertions.assertEquals ("ing",user.getLastName ());
+        Assertions.assertTrue (user.isActive ());
+        Assertions.assertTrue (user.isSubscribedFlashcard ());
+        Assertions.assertTrue (user.isSubscribedStacktrace ());
+        Assertions.assertEquals (authority,user.getAuthority ());
+        Assertions.assertEquals (registrationTime,user.getRegistrationTime ());
+        Assertions.assertEquals (lastLoginTime,user.getLastLogin ());
+    }
+}
