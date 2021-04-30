a0d4c24487504dc4868b9cdc8508b15f714b13f5
Daniel Reyes <35903470+DanRey321@users.noreply.github.com>
2021-04-29 20:46:53 -0700

    User and Batch Get Methods (findBy)

--- src/main/java/com/revature/StudyForce/user/repository/BatchRepository.java
+++ src/main/java/com/revature/StudyForce/user/repository/BatchRepository.java
@@ -3,9 +3,21 @@ package com.revature.StudyForce.user.repository;
 import com.revature.StudyForce.user.model.Batch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.security.Timestamp;
+import java.util.List;
+
+
 /**
  * Basic repository for {@link Batch}
  * @author Daniel Bernier
+ * @author  Daniel Reyes
  */
 public interface BatchRepository extends JpaRepository<Batch,Integer> {
+    Batch findByName(String name);
+    Batch findByCreationTime(Timestamp creation);
+
+    Batch findByNameIgnoreCase(String name);
+
+    //List<Batch> findByCreationDateGreater(Timestamp timestamp);
+
 }
