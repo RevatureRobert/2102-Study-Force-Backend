e04b10ab5083c6809053d283e5be9144cc2a47e1
Jl3E <joey.elmblad@gmail.com>
2021-04-30 11:53:57 -0700

    added comments for Solution where stacktrace fk can be nullable, and took away the @NotNull annotation.

--- src/main/java/com/revature/StudyForce/stacktrace/model/SolutionVote.java
+++ src/main/java/com/revature/StudyForce/stacktrace/model/SolutionVote.java
@@ -8,6 +8,12 @@ import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 
+/**
+ * This model represents the solutions users will supply the Stacktrace owner.
+ * The Stacktrace owner will choose one of these solutions that solves his problem.
+ * @author Joey Elmblad
+ */
+
 @Entity
 @Getter
 @Setter
@@ -15,15 +21,26 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 
 public class SolutionVote {
+
+    /**
+     * This is the primary key for the solution vote, to track if a user has voted once on a solution yet.
+     */
     @Id
     @GeneratedValue
     @Column(name = "solution_vote_id")
     private int id;
 
+    /**
+     *  This is a foreign key for the User table to track which user has posted this possible solution.
+     */
     @ManyToOne
     @JsonIgnore
     @JoinColumn(name = "user_id")
     private User user;
+
+    /**
+     * This is a foreign key for the Solution table which tracks which solution this vote will count towards.
+     */
     @ManyToOne
     @JsonIgnore
     @JoinColumn(name = "solution_id")
