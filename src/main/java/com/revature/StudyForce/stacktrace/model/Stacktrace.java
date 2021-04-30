5cf336f9fa9d7d6765768963b78fc05108a2bcba
nshaji24 <ns35@njit.edu>
2021-04-30 11:04:39 -0400

    adding comments

--- src/main/java/com/revature/StudyForce/stacktrace/model/Stacktrace.java
+++ src/main/java/com/revature/StudyForce/stacktrace/model/Stacktrace.java
@@ -15,26 +15,42 @@ import java.sql.Timestamp;
 @Getter
 @Setter
 @Builder
-public class Stacktrace {
-
+public class Stacktraces {
+    /**
+     * @Author : Noel Shaji
+     * Primary key
+     */
     @Id
     @GeneratedValue
     private int stackstraceId;
 
-    //TODO: change ints to Objects
-    @NotNull
-    private int creatorId;
+    /**
+     * TODO: change ints to Objects once user models are created
+     * ID of user who created stacktrace
+     */
+    @NotNull private int creatorId;
 
+    /**
+     * the title a user gives to a stacktrace
+     */
     @NotBlank
     private String title;
 
+    /**
+     * the body text of a stacktrace
+     */
     @NotBlank
     private String body;
 
-    //TODO: change ints to Objects
-    @NotNull
-    private int technologyId;
+    /**
+     * TODO: change ints to Objects once technology model is added
+     * The technology that the stacktrace is using.ex JAVA
+     */
+    @NotNull private int technologyId;
 
+    /**
+     *The timestamp of when the stacktrace was created
+     */
     @UpdateTimestamp
     private Timestamp creationTime;
 
---
+++ src/main/java/com/revature/StudyForce/user/repository/StacktraceRepository.java
@@ -0,0 +1,2 @@
+package com.revature.StudyForce.user.repository;public interface StacktraceRepository {
+}
