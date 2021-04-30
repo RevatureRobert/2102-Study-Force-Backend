317e993ca713c015709b7debb1fcdef4f294de9d
dannyBernier <46949659+dannyBernier@users.noreply.github.com>
2021-04-30 16:16:37 -0500

    corrected users and userDTO to only have a name

--- src/main/java/com/revature/studyforce/user/model/User.java
+++ src/main/java/com/revature/studyforce/user/model/User.java
@@ -38,13 +38,9 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Size(max = 255)
+    @Size(max = 511)
     @Column(name = "first_name")
-    private String firstName;
-
-    @Size(max = 255)
-    @Column(name = "last_name")
-    private String lastName;
+    private String name;
 
     @Column(name = "is_active")
     private boolean isActive;
