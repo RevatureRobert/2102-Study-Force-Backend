73cefed91fe58acef933e4a86f3c3de983cce56b
dannyBernier <46949659+dannyBernier@users.noreply.github.com>
2021-04-30 11:47:26 -0500

    renamed package to lowercase studyforce

--- src/main/java/com/revature/studyforce/StudyForceApplication.java
+++ src/main/java/com/revature/studyforce/StudyForceApplication.java
@@ -1,4 +1,4 @@
-package com.revature.StudyForce;
+package com.revature.studyforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
