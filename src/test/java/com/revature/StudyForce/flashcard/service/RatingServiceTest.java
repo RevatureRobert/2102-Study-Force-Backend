e71989d7ee649191dae779bbb3fe75e6dac589f8
Luke Mohr <luke.mohr@revature.net>
2021-05-01 16:27:07 -0600

    Removed pom plugin, modified test

--- src/test/java/com/revature/StudyForce/flashcard/service/RatingServiceTest.java
+++ src/test/java/com/revature/StudyForce/flashcard/service/RatingServiceTest.java
@@ -57,7 +57,7 @@ class RatingServiceTest {
 
         RatingResponseDTO res = ratingService.createRating(RatingDTO.ratingToDTO().apply(rating));
         assertNotNull(res);
-        assertTrue(res.getRating()==0);
+        assertEquals(0, res.getRating());
         assertTrue(res.getTotalRatings()>0);
 
         System.out.println(res);
