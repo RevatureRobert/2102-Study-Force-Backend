aa3974682990a8a99ef3e43b01ff9cc33c70b219
pgonzalez017 <42748202+pgonzalez017@users.noreply.github.com>
2021-05-01 15:34:49 -0400

    Took out the User Stuff

--- src/main/java/com/revature/studyforce/notification/repository/UserRepository.java
+++ src/main/java/com/revature/studyforce/notification/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.revature.studyforce.notification.repository;
 
-import com.revature.studyforce.notification.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
