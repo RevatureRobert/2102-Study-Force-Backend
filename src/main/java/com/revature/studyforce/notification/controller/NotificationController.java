1423c0938e12ccb0154802e91be1ebc1131044cb
pgonzalez017 <42748202+pgonzalez017@users.noreply.github.com>
2021-05-02 15:52:27 -0400

    Added Documentation to both the Notification Controller and Notification Service

--- src/main/java/com/revature/studyforce/notification/controller/NotificationController.java
+++ src/main/java/com/revature/studyforce/notification/controller/NotificationController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /***
- * Notification Controller
  * author: Patrick
  */
 
@@ -23,6 +22,12 @@ public class NotificationController {
     @Autowired
     private NotificationService notificationService;
 
+    /***
+     *
+     * @return
+     *     Returns either the entire list of notifications from the database
+     *     or if there are no notifications it returns a not found http response
+     */
     @GetMapping
     public ResponseEntity<List<Notification>> getAllNotifications(){
         List<Notification> notificationList = notificationService.findAll();
@@ -32,17 +37,37 @@ public class NotificationController {
         return ResponseEntity.notFound().build();
     }
 
+    /***
+     * @param userId
+     * userId is the id of the user that we are grabbing the page for
+     * @param page
+     * default value of page is 0 so if no page number is entered
+     * We start with the very first page.
+     * @return
+     *     Method returns a page of notifications
+     *     The default size of the page is 5
+     *     If there are no notifications then
+     *     We return an Http Response of Not Found
+     */
     // If we want the user to specify certain pagination or a certain page then we have
     // to pass it in as a Path Variable and create the corresponding pageable object
     @GetMapping("/{id}")
-    public ResponseEntity<Page<Notification>> getNotificationsByUserId(@PathVariable("id") Integer id, @RequestParam(name="page", defaultValue = "0") String page){
-        Page<Notification> notificationPage = notificationService.findByUserId(id, Integer.parseInt(page));
+    public ResponseEntity<Page<Notification>> getNotificationsByUserId(@PathVariable("id") Integer userId, @RequestParam(name="page", defaultValue = "0") String page){
+        Page<Notification> notificationPage = notificationService.findByUserId(userId, Integer.parseInt(page));
         if(notificationPage != null){
             return ResponseEntity.ok(notificationPage);
         }
         return ResponseEntity.notFound().build();
     }
 
+    /***
+     *
+     * @param notification
+     * A notification is passed into the controller from the client side
+     * @return
+     * We return the same notification if it is inserted successfully into the database
+     * If the notification parameter is null then we return an Http Response of Unprocessable Entity
+     */
     @PostMapping
     public ResponseEntity<Notification> addNotification(@RequestBody Notification notification){
         if(notification != null){
@@ -52,10 +77,18 @@ public class NotificationController {
         return ResponseEntity.unprocessableEntity().build();
     }
 
+    /***
+     *
+     * @param userId
+     * The path should the userId for use
+     * @return
+     * Method returns either a No Content response if the notifications for a user are successfully deleted
+     * or a Not Found Http Response if there weren't any notifications deleted
+     */
     @DeleteMapping("/{id}")
-    public ResponseEntity<Notification> deleteAllNotificationsByUserId(@PathVariable("id") Integer id){
+    public ResponseEntity<Notification> deleteAllNotificationsByUserId(@PathVariable("id") Integer userId){
         try{
-            notificationService.deleteByUserId(id);
+            notificationService.deleteByUserId(userId);
             return ResponseEntity.noContent().build();
         }
         catch(EmptyResultDataAccessException e){
--- src/main/java/com/revature/studyforce/notification/service/NotificationService.java
+++ src/main/java/com/revature/studyforce/notification/service/NotificationService.java
@@ -11,43 +11,101 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/***
+ * author: Patrick
+ */
 @Service
 public class NotificationService {
 
     @Autowired
     NotificationRepository notificationRepository;
 
+    /***
+     * Grab and return the notification based on the id passed
+     * @param id
+     * The id parameter represents the id of the notification in the database
+     * @return
+     * Method returns the notification retrieved from the database
+     * or it returns null if the notification was not found
+     */
     public Notification findById(Integer id){
         Optional<Notification> notification = notificationRepository.findById(id);
         return notification.orElse(null);
     }
 
+    /***
+     * Grab all notifications from the database and return a list of them
+     * @return
+     * Returns a List of all the notifications in the database
+     */
     public List<Notification> findAll(){
         return notificationRepository.findAll();
     }
 
+    /***
+     * Find all notifications that belong to a particular user
+     * and return a page of those notifications
+     * @param userId
+     * We use the userId in order to find and get all of the notifications that correspond to this user
+     * @param page
+     * The page parameter determines what page number we are returning
+     * The default size of the page is always 5 notifications
+     * @return
+     * Method returns a Page of Notifications
+     */
     public Page<Notification> findByUserId(Integer userId, Integer page){
         // We can change the page request parameters later
         return notificationRepository.findByApplicationUserId(userId, PageRequest.of(page, 5, Sort.by("notificationId").descending()));
     }
 
+    /***
+     * Insert a notification into the database
+     * @param notification
+     * Method takes in a notification that is saved in the database
+     */
     public void save(Notification notification){
         notificationRepository.save(notification);
     }
 
+    /***
+     * Update an existing notification
+     * @param notification
+     * Method takes in a notification that updates an existing notification
+     * If the notification does not exist
+     * then we do not enter the notification into the database
+     */
     public void update(Notification notification){
-        notificationRepository.save(notification);
+        Optional<Notification> checkNotification = notificationRepository.findById(notification.getNotificationId());
+        if(checkNotification.isPresent()){
+            notificationRepository.save(notification);
+        }
     }
 
+    /***
+     * Delete a notification
+     * @param notification
+     * notification is the parameter to be deleted from our database
+     */
     public void delete(Notification notification){
         notificationRepository.delete(notification);
     }
 
+    /***
+     * Delete a notification based on the id passed
+     * @param id
+     * id is the id of the notification we want to delete from the database
+     */
     public void deleteById(Integer id){
         notificationRepository.deleteById(id);
     }
 
-    public void deleteByUserId(Integer id){ notificationRepository.deleteByApplicationUserId(id); }
+    /***
+     * Delete all notifications that belong to a particular user
+     * @param userId
+     * userId is the parameter used to find all notifications that belong
+     * to that particular user
+     */
+    public void deleteByUserId(Integer userId){ notificationRepository.deleteByApplicationUserId(userId); }
 
 
 
