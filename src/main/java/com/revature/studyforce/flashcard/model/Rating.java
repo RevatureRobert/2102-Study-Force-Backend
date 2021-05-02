02e7dd9ac1e968c25ff7be16a57e84963d9941c5
DanielKopeloff <79463544+DanielKopeloff@users.noreply.github.com>
2021-05-01 22:19:32 -0400

    Revert "Stacktrace models"

--- src/main/java/com/revature/studyforce/flashcard/model/Rating.java
+++ src/main/java/com/revature/studyforce/flashcard/model/Rating.java
@@ -1,16 +1,21 @@
 package com.revature.studyforce.flashcard.model;
 
-import com.revature.StudyForce.user.model.User;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.revature.studyforce.user.model.User;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 /**
- * Model used to store the difficulty ratings of a flashcards {@link Flashcard}, {@link User}, {@link Difficulty}
+ * Model used to store the difficulty ratings of a flashcards
+ *
  *@author Edson Rodriguez
  */
 @Entity
-@Table(name = "rating")
+@Table(name = "timesheets")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,10 +24,8 @@ public class Rating {
     @GeneratedValue
     private int id;
     @NotNull
-    @ManyToOne
-    private Flashcard flashcard;
+    private Flashcard flashcardId;
     @NotNull
-    @ManyToOne
     private User user;
     @NotNull
     @Column(name = "rating")
