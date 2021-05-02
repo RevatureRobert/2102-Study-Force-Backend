00965c2d274c4539ec18df10fdd634b33774fa77
nwickh03 <79473056+nwickh03@users.noreply.github.com>
2021-05-01 20:01:05 -0400

    Began creating SecurityConfiguration.java

---
+++ src/main/java/com/revature/studyforce/user/configure/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.revature.studyforce.user.configure;
+import com.revature.studyforce.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(username -> (UserDetails) userService.getUserByEmail(username));
+    }
+}
