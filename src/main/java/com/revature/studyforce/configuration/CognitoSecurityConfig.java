0f5012c2a4beccd9b37001b4fb5fa52c704e9f4e
nwickh03 <79473056+nwickh03@users.noreply.github.com>
2021-05-03 09:29:22 -0400

    Security mostly working

--- src/main/java/com/revature/studyforce/configuration/CognitoSecurityConfig.java
+++ src/main/java/com/revature/studyforce/configuration/CognitoSecurityConfig.java
@@ -1,28 +1,40 @@
 package com.revature.studyforce.configuration;
-import com.revature.studyforce.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
-public class CognitoSecurityConfig extends WebSecurityConfigurerAdapter {
+public class CognitoSecurityConfig extends ResourceServerConfigurerAdapter {
+
+    private final ResourceServerProperties resource;
 
     @Autowired
-    private UserService userService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username -> (UserDetails) userService.getUserByEmail(username));
+    public CognitoSecurityConfig(ResourceServerProperties resource) {
+        this.resource = resource;
     }
 
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().oauth2Client().and().authorizeRequests().anyRequest().authenticated();
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated();
     }
+
+    @Bean
+    public TokenStore jwkTokenStore() {
+        return new JwkTokenStore(
+                Collections.singletonList(resource.getJwk().getKeySetUri()),
+                new CognitoAccessTokenConverter(),
+                null);
+    }
+
 
 }
