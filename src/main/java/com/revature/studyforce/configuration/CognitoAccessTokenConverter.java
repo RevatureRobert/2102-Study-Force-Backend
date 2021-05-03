8905189c94e1a4ee8caaf43827e49d7342ce8312
nwickh03 <79473056+nwickh03@users.noreply.github.com>
2021-05-03 14:44:16 -0400

    Revert "broken"
    This reverts commit 24f0802232183c2125da5e764a4dded5abe28ac8.

--- src/main/java/com/revature/studyforce/configuration/CognitoAccessTokenConverter.java
+++ src/main/java/com/revature/studyforce/configuration/CognitoAccessTokenConverter.java
@@ -1,15 +1,8 @@
 package com.revature.studyforce.configuration;
 
-import com.revature.studyforce.cognito.service.CognitoService;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.stereotype.Component;
-
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.CognitoIdentityProviderException;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.CreateUserPoolRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.CreateUserPoolResponse;
 
 import java.util.Map;
 
@@ -20,12 +13,9 @@ public class CognitoAccessTokenConverter extends JwtAccessTokenConverter {
     private static final String COGNITO_USERNAME = "username";
     private static final String SPRING_USER_NAME = "user_name";
 
-
-
     @SuppressWarnings("unchecked")
     @Override
     public OAuth2Authentication extractAuthentication(Map<String, ?> claims) {
-
 
         if (claims.containsKey(COGNITO_GROUPS))
             ((Map<String, Object>) claims).put(SPRING_AUTHORITIES, claims.get(COGNITO_GROUPS));
