8b0804241521ebd8707f70d2ce2965af23e57548
Ronald (Jeff) Lopez <32562609+lopezjronald@users.noreply.github.com>
2021-05-03 21:59:18 -0700

    added delete notification method in notification repository

--- src/main/java/com/revature/StudyForce/notification/repository/NotificationRepository.java
+++ src/main/java/com/revature/StudyForce/notification/repository/NotificationRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RepositoryRestResource
+import java.util.Date;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "notifications", path = "notifications")
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     Page<Notification> findByApplicationUserId(@RequestParam("id") Integer id, Pageable pageable);
+    Page<Notification> deleteByNotificationId(@RequestParam("notificationId") Integer id, Pageable pageable);
+    Page<Notification> findByIsReadAndTimeToLiveBetween(@RequestParam("isRead") Boolean isRead, @RequestParam("timeToLive") @DateTimeFormat(pattern="yyy-MM-dd") Date dateCreated, @RequestParam("timeToLive") @DateTimeFormat(pattern="yyy-MM-dd") Date expirationDate, Pageable pageable);
 }
