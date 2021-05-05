ede743298582d8d0e9e2e9aa612bc6406d17fa57
nshaji24 <ns35@njit.edu>
2021-05-05 17:57:10 -0400

    documentation only

--- src/test/java/com/revature/studyforce/stacktrace/controller/StackTraceControllerTest.java
+++ src/test/java/com/revature/studyforce/stacktrace/controller/StackTraceControllerTest.java
@@ -35,7 +35,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *
  * @author John Stone
  * @author Joshua Swanson
- * @author Noel Shaji
  */
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
