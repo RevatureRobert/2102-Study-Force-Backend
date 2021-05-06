1c3d7d86b1c4c74c00b49513f92a7eb8b51e2ca9
Jl3E <joey.elmblad@gmail.com>
2021-05-06 12:33:53 -0700

    revert

--- src/main/java/com/revature/studyforce/user/repository/BatchRepository.java
+++ src/main/java/com/revature/studyforce/user/repository/BatchRepository.java
@@ -1,11 +1,22 @@
 package com.revature.studyforce.user.repository;
 
 import com.revature.studyforce.user.model.Batch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
 
 /**
  * Basic repository for {@link Batch}
  * @author Daniel Bernier
+ * @author  Daniel Reyes
  */
+@Repository
 public interface BatchRepository extends JpaRepository<Batch,Integer> {
+
+    Page<Batch> findByCreationTimeAfter(Timestamp creation, Pageable pageable);
+
+    Batch findByNameContainingIgnoreCase(String name);
 }
