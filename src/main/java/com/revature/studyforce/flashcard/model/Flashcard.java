4bf4ae684bd06574b48330738d3154542971f966
Nate Ford <nathaniel.ford@revature.net>
2021-05-06 17:59:46 -0500

    get ready to revert this if it isn't perfect

--- src/main/java/com/revature/studyforce/flashcard/model/Flashcard.java
+++ src/main/java/com/revature/studyforce/flashcard/model/Flashcard.java
@@ -1,13 +1,13 @@
 package com.revature.studyforce.flashcard.model;
 
-
-import com.revature.studyforce.user.model.User;
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+
+import com.revature.studyforce.user.model.User;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -29,14 +29,15 @@ public class Flashcard {
     @NotNull
     @ManyToOne
     private User creator;
-    @ManyToOne
-    private Topic topic;
+    //TODO: waiting on Topic model
+//    @NotNull
+//    @ManyToOne
+//    private Topic topic;
     @Column(nullable = false)
     private String question;
     private int questionDifficultyTotal = 0;
     private int questionDifficultyAverage = 0;
     private Timestamp createdTime = Timestamp.valueOf(LocalDateTime.now());
     private Timestamp resolutionTime = Timestamp.valueOf(LocalDateTime.now());
-    private boolean isResolved = false;
 
 }
---
+++ src/main/java/com/revature/studyforce/user/controller/BatchController.java
@@ -0,0 +1,79 @@
+package com.revature.studyforce.user.controller;
+
+import com.revature.studyforce.user.model.Batch;
+import com.revature.studyforce.user.service.BatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Batch Controller for Batch Repo {@link BatchService}
+ * @author Daniel Reyes
+ * @author Daniel Bernier
+ */
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/batches")
+public class BatchController {
+
+    private final BatchService batchService;
+
+    @Autowired
+    public BatchController(BatchService batchService){
+        this.batchService = batchService;
+    }
+
+    /**
+     * GET mapping for '/allBatches' uses {@link BatchService#getAllBatches(int, int, String, String)}
+     * @param sortBy field to be sorted by [id | time | name] case insensitive defaults to id
+     * @param order type of order to sort batches [asc | desc] case insensitive - defaults to asc
+     * @param page page to be displayed [page >= 0] defaults to 5
+     * @param offset number of batches displayed per page [5/ 10/ 25/ 50] defaults to 5 if invalid
+     * @return a page of batches dependent on provided page, offset, sort, and order params.
+     */
+    @GetMapping
+    public Page<Batch> getAllBatches(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                @RequestParam(value = "offset", required = false, defaultValue = "10") int offset,
+                                @RequestParam(value = "sort", required = false, defaultValue = "batchId") String sortBy,
+                                @RequestParam(value = "order", required = false, defaultValue = "Desc") String order) {
+        return batchService.getAllBatches(page, offset, sortBy, order);
+    }
+
+    /**
+     * GET mapping for 'getBatchByCreationTime' in {@link BatchService#getBatchByCreationTime(long, int, int, String, String)}
+     * @param timestamp timestamp in long format
+     * @param sortBy field to be sorted by [id | time | name]  case insensitive defaults to batchId
+     * @param order type of order to sort batches [asc | desc] case insensitive - defaults to asc
+     * @param page page to be displayed [page >= 0] defaults to 5
+     * @param offset number of batches displayed per page [5 | 10 | 25| 50] defaults to 5 if invalid
+     * @return a page of batches dependent on provided page, offset, sort, and order params.
+     */
+    @GetMapping("/time/{epochMilliseconds}")
+    public Page<Batch> getBatchByCreationTime(@PathVariable("epochMilliseconds") long timestamp,
+                                              @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                              @RequestParam(value = "offset", required = false, defaultValue = "10") int offset,
+                                              @RequestParam(value = "sort", required = false, defaultValue = "batchId") String sortBy,
+                                              @RequestParam(value = "order", required = false, defaultValue = "ASC") String order){
+        return batchService.getBatchByCreationTime(timestamp, page,offset,sortBy,order);
+    }
+
+    /**
+     * GET request for 'getBatchByName' in {@link BatchService#getBatchByName(String)}
+     * @param name String input to represent a batch name
+     * @return single batch that matches that name
+     */
+    @GetMapping("/name")
+    public Batch getBatchByBatchName(@RequestParam(name = "name") String name){
+        return batchService.getBatchByName(name);
+    }
+
+    /**
+     * GET request for 'getBatchById' in {@link BatchService#getBatchById(int)}
+     * @param batchId int input to represent a batch id
+     * @return single batch return that matches batchId param.
+     */
+    @GetMapping("/{batchId}")
+    public Batch getBatchByBatchId(@PathVariable(name = "batchId") int batchId){
+        return batchService.getBatchById(batchId);
+    }
+}
