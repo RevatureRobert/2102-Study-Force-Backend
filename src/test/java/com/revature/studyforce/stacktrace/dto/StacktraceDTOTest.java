a9ccc4b87dc2ba633a2fd6bc32f66554e2e74518
Jl3E <joey.elmblad@gmail.com>
2021-05-06 18:48:57 -0700

    dto test

--- src/test/java/com/revature/studyforce/stacktrace/dto/StacktraceDTOTest.java
+++ src/test/java/com/revature/studyforce/stacktrace/dto/StacktraceDTOTest.java
@@ -17,11 +17,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @TestPropertySource(locations = "classpath:test-application.properties")
 
 class StacktraceDTOTest {
+
     @Test
     void whenStackTraceToDTOThenCorrectDTOReturned () {
-        StacktraceDTO dto = new StacktraceDTO(1,1,"Bob","TestTitle","TestBody",4,"TestTech",new Timestamp(0));
-        Stacktrace s = new Stacktrace(1,new User(3,"Test@mail.com","Bob",true,true,true, Authority.USER,new java.sql.Timestamp(0),new Timestamp(0)),
+        Stacktrace s = new Stacktrace(1,new User(1,"Test@mail.com","Bob",true,true,true, Authority.USER,new java.sql.Timestamp(0),new Timestamp(0)),
                 "TestTitle","TestBody",new Technology(4,"TestTech"),new Timestamp(0),null);
+        StacktraceDTO dto = new StacktraceDTO(1,1,"Bob","TestTitle","TestBody",4,"TestTech",new Timestamp(0));
         StacktraceDTO result = StacktraceDTO.stacktraceToDTO().apply(s);
         assertEquals(dto.getUserId(),result.getUserId());
         assertEquals(dto.getUserName(),result.getUserName());
