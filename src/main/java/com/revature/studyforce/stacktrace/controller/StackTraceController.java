7d37af5c52d21e4a67c841a4b9d3702841a29b2b
nshaji24 <ns35@njit.edu>
2021-05-07 10:56:49 -0400

    functionality with front end

--- src/main/java/com/revature/studyforce/stacktrace/controller/StackTraceController.java
+++ src/main/java/com/revature/studyforce/stacktrace/controller/StackTraceController.java
@@ -1,10 +1,14 @@
 package com.revature.studyforce.stacktrace.controller;
 
 import com.revature.studyforce.stacktrace.dto.StacktraceDTO;
+import com.revature.studyforce.stacktrace.model.Stacktrace;
+import com.revature.studyforce.stacktrace.repository.StacktraceRepository;
 import com.revature.studyforce.stacktrace.service.StacktraceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Rest controller to handle stacktrace requests
@@ -13,12 +17,15 @@ import org.springframework.web.bind.annotation.*;
  * @author Joshua Swanson
  */
 @RestController
-@CrossOrigin(origins = "*")
-@RequestMapping("/stacktrace")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping
 public class StackTraceController {
 
     @Autowired
     private StacktraceService stacktraceService;
+
+    @Autowired
+    private StacktraceRepository stacktraceRepository;
 
     /**
      * Gets all Stacktraces using {@link StacktraceService#getAllStacktraces(int, int)}
@@ -26,12 +33,17 @@ public class StackTraceController {
      * @param pageSize Number of {@link StacktraceDTO StacktraceDTOs} to be returned
      * @return Page of {@link StacktraceDTO StacktraceDTOs} according to pagesize
      */
-    @GetMapping()
+    /*@GetMapping("/stacktrace")
     public Page<StacktraceDTO> getAllStackTraces(@RequestParam(value = "page", defaultValue = "0", required = false) int page,
                                                  @RequestParam(value = "pageSize", defaultValue = "5", required = false) int pageSize) {
         return stacktraceService.getAllStacktraces(page, pageSize);
-    }
+    }*/
 
+
+    @GetMapping("/stacktrace")
+    public List<Stacktrace> getStacktraces() {
+        return (List<Stacktrace>) stacktraceRepository.findAll();
+    }
     /**
      * Gets stacktrace who's id matches provided id using {@link StacktraceService#getStackTraceById(int)}
      * @param id The id of the stacktrace requested
@@ -58,7 +70,7 @@ public class StackTraceController {
      * @param stacktraceDTO the DTO of stacktrace as an object
      * @return The data transfer representation of the created Stacktrace
      */
-    @PostMapping
+    @PostMapping("/stacktrace")
     public StacktraceDTO createStacktrace(@RequestBody StacktraceDTO stacktraceDTO) {
         return stacktraceService.submitStackTrace(stacktraceDTO);
     }
